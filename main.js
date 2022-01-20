3b20973db0bb797acfe37e30a9d0c0c79ec2791b
fernandobouchet <fernandobouchet@gmail.com>
2022-01-19 23:48:59 -0300

    Added some css styles and some clear the previous created card before create a new one

--- main.js
+++ main.js
@@ -3,17 +3,21 @@ const cityInput = document.getElementById("city-input");
 const weatherDiv = document.getElementById("weather-display");
 
 searchButton.addEventListener("click", () => {
+  removeWeatherCard();
   getWeather(cityInput.value).then((result) => createWeatherCard(result));
 });
 
 async function getWeather(city) {
-  const getApi = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cad0610116d54da5c37d79b27bdee371`,
-    { mode: "cors" }
-  );
-  const weatherData = await getApi.json();
-  console.log(weatherData);
-  return weatherData;
+  try {
+    const getApi = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=cad0610116d54da5c37d79b27bdee371`,
+      { mode: "cors" }
+    );
+    const weatherData = await getApi.json();
+    return weatherData;
+  } catch (error) {
+    alert("error");
+  }
 }
 
 function createWeatherCard(city) {
@@ -25,29 +29,31 @@ function createWeatherCard(city) {
   const minTemperature = document.createElement("h3");
   const maxTemperature = document.createElement("h3");
   const humidity = document.createElement("h3");
-  //  const windSpeed = document.createElement("h3");
-  const main = document.createElement("h3");
   const description = document.createElement("h3");
   const icon = document.createElement("img");
   cityName.textContent = city.name;
-  temperature.textContent = `Temperature: ${city.main.temp}`;
-  feelsLikeTemperature.textContent = `Feels like: ${city.main.feels_like}`;
-  minTemperature.textContent = `Min: ${city.main.temp_min}`;
-  maxTemperature.textContent = `Max: ${city.main.temp_max}`;
+  temperature.textContent = `Temperature: ${city.main.temp}°C`;
+  feelsLikeTemperature.textContent = `Feels like: ${city.main.feels_like}°C`;
+  minTemperature.textContent = `Min: ${city.main.temp_min}°C`;
+  maxTemperature.textContent = `Max: ${city.main.temp_max}°C`;
   humidity.textContent = `Humidity: ${city.main.humidity}%`;
   //  windSpeed.textContent = `Wind speed : ${city.wind.speed}`;
-  main.textContent = `Main: ${city.weather[0].main}`;
-  description.textContent = `Description: ${city.weather[0].description}`;
+  description.textContent = `"${city.weather[0].description}"`;
   icon.src = `http://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`;
   weatherContainer.appendChild(cityName);
-  weatherContainer.appendChild(main);
+  weatherContainer.appendChild(icon);
   weatherContainer.appendChild(description);
   weatherContainer.appendChild(temperature);
   weatherContainer.appendChild(feelsLikeTemperature);
   weatherContainer.appendChild(minTemperature);
   weatherContainer.appendChild(maxTemperature);
   weatherContainer.appendChild(humidity);
-  //  weatherContainer.appendChild(windSpeed);
-  weatherContainer.appendChild(icon);
   weatherDiv.appendChild(weatherContainer);
+}
+
+function removeWeatherCard() {
+  const weatherContainer = document.getElementById("weather-container");
+  if (document.contains(weatherContainer)) {
+    weatherDiv.removeChild(weatherContainer);
+  }
 }
